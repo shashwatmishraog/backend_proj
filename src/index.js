dd191e39109ef7fadbb1d740b73ade51f699a24e
shashwat mishra <shashwatmishraog@gmail.com>
2024-08-04 14:53:26 +0530

    api-creation

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`server is running on port: ,${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed !!",err);
+})
